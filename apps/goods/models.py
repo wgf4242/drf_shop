37631b0e077032b35dcd3287de5e26edf8c5dbc2
wgf4242 <wgf4242@gmail.com>
2018-01-13 17:35:36 +0800

    3-8 import categories data from json object

--- apps/goods/models.py
+++ apps/goods/models.py
@@ -39,7 +39,7 @@ class GoodsCategoryBrand(models.Model):
     category = models.ForeignKey(GoodsCategory, null=True, blank=True, verbose_name="商品类目", on_delete=models.CASCADE)
     name = models.CharField(default="", max_length=30, verbose_name="品牌名", help_text="品牌名")
     desc = models.TextField(default="", max_length=200, verbose_name="品牌描述", help_text="品牌描述")
-    image = models.ImageField(max_length=200, upload_to="brand/images")
+    image = models.ImageField(max_length=200, upload_to="brands/")
     add_time = models.DateField(default=datetime.now, verbose_name="添加时间")
 
     class Meta:
@@ -67,7 +67,7 @@ class Goods(models.Model):
     goods_desc = UEditorField(verbose_name="内容", imagePath="goods/images/", width=1000, height=300,
                               filePath="goods/files/", default="")
     ship_free = models.BooleanField(default=True, verbose_name="是否承担运费")
-    goods_front_image = models.ImageField(upload_to="", null=True, blank=True, verbose_name="封面图")
+    goods_front_image = models.ImageField(upload_to="goods/images/", null=True, blank=True, verbose_name="封面图")
     is_new = models.BooleanField(default=False, verbose_name="是否新品")
     is_hot = models.BooleanField(default=False, verbose_name="是否热销")
     add_time = models.DateField(default=datetime.now, verbose_name="添加时间")
